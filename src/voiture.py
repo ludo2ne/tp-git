f8018a9ffad15bde1dd7d1a4e0ddb56b9623fa95
ludo2ne <ludovic.deneuville@ensai.fr>
2025-10-01 09:11:15 +0000

    t8

--- src/voiture.py
+++ src/voiture.py
@@ -12,7 +12,7 @@ class Voiture:
     """
 
     def __init__(self, nom, couleur):
-        """Constructeqfsdur.   """
+        """Constrfsfdsfucteqfsdur.   """
         self.nom = nom
         self.couleur = couleur
         self.vitesse = 0
