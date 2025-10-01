962b4e912a7c66399bc11faa88b420bbd3a0f45b
ludo2ne <ludovic.deneuville@ensai.fr>
2025-10-01 13:38:26 +0000

    ci1

---
+++ src/test_voiture.py
@@ -0,0 +1,23 @@
+from voiture import Voiture
+
+class TestVoiture:
+
+    def test_accelere_incremente_vitesse():
+        # GIVEN
+        v = Voiture("4L", "verte")
+        
+        # WHEN
+        v.accelere(5)
+        
+        # THEN
+        assert v.vitesse == 5
+
+    def test_accelere_limite_increment():
+        # GIVEN
+        v = Voiture("4L", "verte")
+        
+        # WHEN
+        v.accelere(20)
+        
+        # THEN
+        assert v.vitesse == 10
